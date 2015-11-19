63a3ca83593397c7103144e97a861e883de5d1f5
hafenr <robin.hafen@gmail.com>
2015-11-19 14:29:16 +0100

    Update script to use new MACode functions

--- manual_complex_annotation/mergeManualAnnotations.R
+++ manual_complex_annotation/mergeManualAnnotations.R
@@ -1,4 +1,5 @@
 require(devtools)
+require(ggplot2)
 # MACode git commit: f80708bd55d2d7724573a4d56599d078ce27e736
 devtools::load_all('~/Dev/MACode')
 
@@ -23,68 +24,21 @@ manual.annotations <- mergeManualComplexAnnotations(manual.annotations.1,
 detected.features <-
     fread('~/Dev/cprophet/run-674/iteration-0000/sec_complexes.tsv')
 
-# All features (detected as well as theoretically true) without the TNs
-all.features <- assessComplexFeatures(
-    true.positive.features=manual.annotations,
-    detected.features=detected.features,
-    feature.vicinity.tol=5
-)
-
-# Same as recall
-calcTPR <- function(dt) {
-    fn <- dt[type == 'FN', .N]
-    tp <- dt[type == 'TP', .N]
-    tp / (tp + fn)  # TP / P 
-}
-calcFPR <- function(dt) {
-    fp <- dt[type == 'FP', .N]
-    tn <- dt[type == 'TN', .N]
-    fp / (tn + fp)  # FP / N
-}
-
-makeROC <- function(detected.features,
-                    true.positive.features,
-                    cutoffs,
-                    feature.vicinity.tol=5) {
-    fpr <- numeric(length=length(cutoffs))
-    tpr <- numeric(length=length(cutoffs))
-    for (i in seq_along(cutoffs)) {
-        cat(sprintf('Calculating TPR/FPR. Iteration: %d\n', i))
-        cval <- cutoffs[i]
-        detected.features.filtered <-
-            detected.features[apex_apmw_fit < cval, list(complex_id, center_rt)]
-        if (nrow(detected.features.filtered) != 0) {
-            all.features <- assessComplexFeatures(
-                true.positive.features=true.positive.features,
-                detected.features=detected.features.filtered,
-                min.rt=3,
-                max.rt=84,
-                feature.vicinity.tol=5
-            )
-            fpr[i] <- calcFPR(all.features)
-            tpr[i] <- calcTPR(all.features)
-        } else {
-            fpr[i] <- 0
-            tpr[i] <- 0
-        }
-    }
-    fpr.order <- order(fpr)
-    list(FPR=fpr[fpr.order], TPR=tpr[fpr.order])
-}
-
 cutoffs <- seq(
     min(detected.features$apex_apmw_fit),
     max(detected.features$apex_apmw_fit),
-    length=100
+    length=10
 )
-res <- makeROC(detected.features, manual.annotations, cutoffs)
 
-plot(res$FPR[order(res$FPR)], res$TPR[order(res$FPR)],
-     ylim=0:1,
-     xlim=0:1,
-     type='o',
-     xlab='FPR | (1 - specificity)', ylab='TPR | sensitivity')
-abline(a=0, b=1, lty=2)
+res <- makeROC(detected.features, manual.annotations, cutoffs)
+plotROC(res)
+
+# p <- p +
+#     scale_x_continuous(limits=c(0, 1)) +
+#     scale_y_continuous(limits=c(0, 1))
+# print(p)
+
+# abline(a=0, b=1, lty=2)
 
 # tp.rate = sapply(cutoffs, calcTFR)
 # fp.rate = FP / N = FP / (TN + FP)  
@@ -92,4 +46,16 @@ abline(a=0, b=1, lty=2)
 # calcRecall <- function(assessed.feats, )
 
 
+# all.features <- assessComplexFeatures(
+#     true.positive.features=manual.annotations,
+#     detected.features=detected.features[apex_apmw_fit < 10000, ],
+#     min.rt=3,
+#     max.rt=84,
+#     feature.vicinity.tol=5
+# )
+# n.points = all.features[type == 'FN', .N] + 
+#            all.features[type == 'TP', .N] + 
+#            all.features[type == 'FP', .N] + 
+#            all.features[type == 'TN', .N] 
+# stopifnot(n.points == nrow(all.features))
 
